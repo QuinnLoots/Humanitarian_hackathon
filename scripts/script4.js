d63e39af81896f27721f047ee7ba87153e25e8cf
Quinn Loots <loots.quinn@gmail.com>
2019-01-16 08:00:36 +0100

    put amounts in variable

--- scripts/script4.js
+++ scripts/script4.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
   $("#tekst").text("Amount of "+ localStorage.getItem("product"));
+  $( "#subm" ).click(function() {
+
   
+    let str = localStorage.getItem("product")+","+$("#amount").val();
+    localStorage.setItem("test",localStorage.getItem("test")+";"+str);
+    
+   
+  });
 });
